d8dda19f1ca6a2b19593d22a6ba6822811cdf82b
Asus <Asus@DESKTOP-HBG9NFH>
2021-01-06 22:43:28 +0330

    Create Course Model

---
+++ Clean_Arch/CleanArch.Domain/Model/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Model
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
